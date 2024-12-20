77a0735bc951c3b109cd5e90846a8ac10b8e4e85
Shabbir Ahamed <shabbirkimgbrand55@gmail.com>
2024-12-20 23:43:39 +0600

    use Tailwind and DaisyUI for style.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className='underline font-bold'>hello bd</h1>
+        <button className="btn">Button</button>
+      </div>
+
+    </>
+  )
+}
+
+export default App
