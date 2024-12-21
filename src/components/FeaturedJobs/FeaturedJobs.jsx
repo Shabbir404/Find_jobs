5f2ee22817af56d44deaadafc1fdf1010dd34341
Shabbir Ahamed <shabbirkimgbrand55@gmail.com>
2024-12-21 22:49:59 +0600

    Home page ready

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -13,24 +13,17 @@ const FeaturedJobs = () => {
     return (
         <div>
             {/* featured jobs singles show */}
-            {
-                jobs.map(job => <Job key={job.id} job={job}> </Job>
-
-                )
-            }
-            <div>
-                <img src="" alt="" />
-                <h2></h2>
-                <span></span>
-                <div>
-                    <Link></Link>
-                    <Link></Link>
-                </div>
-                <div>
-                    <span></span>
-                    <span></span>
-                </div>
-                <Link></Link>
+            <h1 className='font-bold text-center text-5xl mb-4 mt-10'>Featured Jobs</h1>
+            <p className='text-gray-400 text-center mb-10 '>Explore thousands of job opportunities with all the information you need.
+                Its your future</p>
+            <div className='grid gap-4 grid-cols-2 '>
+                {
+                    jobs.map(job => <Job key={job.id} job={job}> </Job>)
+                }
+            </div>
+            <div className='text-center'>
+                <Link className='btn mb-5 text-white bg-gradient-to-r
+                from-indigo-500 via-purple-500 to-pink-500 mt-4 text-center'>See all jobs</Link>
             </div>
         </div>
     );
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,9 +1,27 @@
+import { IoLocationOutline } from "react-icons/io5";
+import { RiMoneyDollarCircleLine } from "react-icons/ri";
 
 const Job = ({ job }) => {
     return (
-        <div>
+        <div className="w-auto h-auto">
 
-            <img src={job.logo} alt="" />
+            <div className="border p-4 mt-2 ">
+                <img src={job.logo} alt="" />
+                <h2 className="mt-5 font-bold text-2xl text-gray-600">{job.job_title}</h2>
+                <p className="mt-2">{job.company_name}</p>
+                <div className="mt-2 flex">
+                    <button className="btn btn-outline btn-primary">{job.remote_or_onsite}</button>
+                    <button className="btn btn-outline btn-primary ml-4">{job.job_type}</button>
+                </div>
+                <div className="mt-4 flex text-gray-600">
+                    <p className="flex gap-2"><IoLocationOutline className="mt-0.5"></IoLocationOutline>{job.location}</p>
+                    <p className="ml-5 flex"><RiMoneyDollarCircleLine className="mt-0.5 mr-1"></RiMoneyDollarCircleLine>
+                        {job.salary}</p>
+                </div>
+                <button className="btn text-white bg-gradient-to-r
+                from-indigo-500 via-purple-500 to-pink-500 mt-4">Viwe Details</button>
+            </div>
+
 
         </div>
     );
